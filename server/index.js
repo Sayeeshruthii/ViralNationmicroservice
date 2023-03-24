ba6ebab34da23517051d9ae9495468351e3202a6
sayeeshruthiwindsor <ananthas@uwindsor.ca>
2023-03-23 22:04:50 -0400

    Port number change

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express(); 
-var port =process.env.PORT || 3001; 
+var port =process.env.PORT || 8080; 
 var routes = require('../routes');
 routes(app); 
 app.listen(port,function(){ console.log('Server started on port: ' + port); });
