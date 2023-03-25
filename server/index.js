27eef86ae6e4bc7c0c25bb44a7758a6f4cbfdd69
sayeeshruthiwindsor <ananthas@uwindsor.ca>
2023-03-25 12:16:58 -0400

    container image change and cors added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
+let cors = require("cors");
+
 var app = express(); 
 var port =process.env.PORT || 8080; 
+app.use(cors());
 var routes = require('../routes');
 routes(app); 
 app.listen(port,function(){ console.log('Server started on port: ' + port); });
